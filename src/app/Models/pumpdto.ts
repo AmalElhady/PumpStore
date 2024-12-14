b45f4531af4244b4c222f5a9b2cd6f5a7f22bc0d
amalelhady <amalelhady@gmail.com>
2024-12-14 12:59:05 +0200

    addCRUDS

---
+++ src/app/Models/pumpdto.ts
@@ -0,0 +1,11 @@
+export class Pumpdto {
+    productId=0;
+    productName="";
+    model="";
+    imageURL="";
+    construction="";
+    inletSize=0;
+    outletSize=0;
+    documentId=0;
+    documentation="";
+}
